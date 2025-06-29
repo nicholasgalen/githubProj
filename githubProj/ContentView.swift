9c44f473fa2d07c524bcc4b57d6ed4801d5b8fc8
Nicholas Galen <galennicholas@proton.me>
2025-06-29 12:10:59 -0400

    Initial Commit

---
+++ githubProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubProj
+//
+//  Created by Ana Paula Marcal Franzoi on 29/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubProj/githubProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubProjApp.swift
+//  githubProj
+//
+//  Created by Ana Paula Marcal Franzoi on 29/06/25.
+//
+
+import SwiftUI
+
+@main
+struct githubProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
